20e7d9f567c452c5018170924a91d83e1e66b22e
Sachin <sachinthampan.mail@gmail.com>
2024-04-10 19:58:36 +0530

    - form reset after auth error

--- lib/main.dart
+++ lib/main.dart
@@ -110,25 +110,30 @@ class _LoginPageState extends State<LoginPage> {
                           child: TextFormField(
                               onChanged: state.setUserName,
                               decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText: "Please enter your username"
-                            ),
-                            validator: (value){
-                              //context.read<Loginbloc>().add(Reset());
-                              return Validators.validateEmail(value);
-                            }
-                            ),),
+                                  border: OutlineInputBorder(),
+                                  hintText: "Please enter your username"
+                              ),
+                              onTap: () {
+                                context.read<Loginbloc>().add(Reset());
+                              },
+                              validator: (value){
+                                return Validators.validateEmail(value);
+                              }
+                          ),),
                         Padding(padding: const EdgeInsets.symmetric(horizontal: 48, vertical: 16),
                           child: TextFormField(
-                            onChanged: state.setPassword,
-                            obscureText: true,
-                            enableSuggestions: false,
-                            autocorrect: false,
-                            decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText: "Please enter your password"
-                            ),
-                            validator: Validators.validatePassword),),
+                              onChanged: state.setPassword,
+                              obscureText: true,
+                              enableSuggestions: false,
+                              autocorrect: false,
+                              decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: "Please enter your password"
+                              ),
+                              onTap: () {
+                                context.read<Loginbloc>().add(Reset());
+                              },
+                              validator: Validators.validatePassword),),
                         OverflowBar(
                           alignment: MainAxisAlignment.center,
                           children: <Widget>[
@@ -153,36 +158,21 @@ class _LoginPageState extends State<LoginPage> {
                                   context.read<Loginbloc>().add(LoginPressed(state.userName!, state.password!));
                                 }
                                 context.read<Loginbloc>().add(Reset());
-                                //context.read<Loginbloc>().add(LoginPressed("hbingley1", "CQutx25i8r"));
                               },
                             )
-                            /*TextButton(
-                              style: ButtonStyle(
-                                backgroundColor:
-                              ),
-                              onPressed: (){
-                                context.read<Loginbloc>().add(LoginPressed("hbingley1", "CQutx25i8r"));
-                              },
-                              child: const Text("Login"),
-                            ),*/
                           ],
                         ),
                         const SizedBox(height: 20.0,),
                         state.loginState == LoginPageStates.LOGIN_FAILED ?
-                          const Text("Login Failed", style: TextStyle(color: Colors.redAccent)) :
+                        const Text("Login Failed", style: TextStyle(color: Colors.redAccent)) :
                         state.loginState == LoginPageStates.LOGIN_SUCCESS ?
-                          const Text("Login Success", style: TextStyle(color: Colors.greenAccent)) :
-                          const SizedBox(height: 10,)
+                        const Text("Login Success", style: TextStyle(color: Colors.greenAccent)) :
+                        const SizedBox(height: 10,)
 
                       ],
                     ),
                   ),
                 ),
-                /*floatingActionButton: FloatingActionButton(
-                    onPressed: () => context.read<Mybloc>().add(RegisterPressed()),
-                    tooltip: 'Increment',
-                    child: const Icon(Icons.add),
-                  )*/
               );
             })
 
@@ -190,57 +180,3 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
-/*@override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );*/
-
